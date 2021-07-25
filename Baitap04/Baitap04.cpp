92609770ffcc39ff850a078e1f2884b1ef97c498
kynm0211 <minhky.book@gmail.com>
2021-07-25 17:52:20 +0700

    them bai tap tinh tien dien

--- Baitap04/Baitap04.cpp
+++ Baitap04/Baitap04.cpp
@@ -11,17 +11,38 @@ int main()
 	remainder = n % 2;
 	switch (remainder)
 	{
-		case 0: 
-		{
-			cout << "So chan";
-		}
-			  break;
-		case 1:
-		{
-			cout << "So le";
-		}
-		default:
-			break;
+	case 0:
+	{
+		cout << "So chan" << endl;
 	}
+	break;
+	case 1:
+	{
+		cout << "So le" << endl;
+	}
+	default:
+		break;
+	}
+	//T¨ªnh ti?n ?i?n d?a v¨¤o s? kwh
+	int kwh, fee;
+	const int kwh100 = 600;
+	const int kwh150 = 700;
+	const int kwh200 = 900;
+	const int kwhMax = 1100;
+	cout << "Tinh tien dien" << endl;
+	cout << "Nhap so kwh da su dung: ", cin >> kwh;
+	if (kwh <= 100) {
+		fee = kwh * 600;
+	}
+	else if (kwh >= 101 && kwh <= 150) {
+		fee = (kwh - 100) * 700 + 100 * 600;
+	}
+	else if (kwh > 150 && kwh <= 200) {
+		fee = (kwh - 150) * 900 + (kwh - 100) * 700 + 100 * 600;
+	}
+	else
+	{
+		fee = (kwh - 200) * 1100 + (kwh - 150) * 900 + (kwh - 100) * 700 + 100 * 600;
+	}
+	cout << "So tong so tien la: " << fee;
 }
-
